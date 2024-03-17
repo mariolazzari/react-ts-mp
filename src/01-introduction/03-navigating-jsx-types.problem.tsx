92e4f5b3b7f1f515f25f70bc0ac40d6523b96a2f
Mario Lazzari <mario.lazzari@gmail.com>
2024-03-17 14:23:27 +0100

    Done lesson 4

---
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -0,0 +1,14 @@
+export const Component = () => {
+  return (
+    <div
+      // How do I figure out what type aria-posinset expects?
+      aria-posinset={1}
+      // How do I figure out what type onChange expects?
+      onChange={e => {
+        console.log(e);
+      }}
+
+      // How do I get autocomplete with JSX?
+    />
+  );
+};
---
+++ src/02-components/04-typing-components.problem.tsx
@@ -0,0 +1,18 @@
+type ButtonProps = {
+  className: string;
+};
+
+export const Button = (props: ButtonProps) => {
+  return <button className={props.className}></button>;
+};
+
+const Parent = () => {
+  return (
+    <>
+      {/* @ts-expect-error */}
+      <Button></Button>
+
+      <Button className="my-class"></Button>
+    </>
+  );
+};
